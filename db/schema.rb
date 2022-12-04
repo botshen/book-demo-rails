503645fcb6a86d885028c31dc4c04e9b5675382c
shenxin <botzgbj@gmail.com>
2022-12-04 21:56:07 +0800

    创建关联

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_004950) do
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
+ActiveRecord::Schema[7.0].define(version: 2022_12_04_133037) do
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,13 +1,43 @@
 require "rails_helper"
 
 describe "Books Api", type: :request do
-  it "returns all books" do
-    FactoryBot.create(:book, title: "Refactoring", author: "Martin")
-    FactoryBot.create(:book, title: "sdewcf", author: "dwdwd")
+  describe "GET /books" do
+    before do
+      FactoryBot.create(:book, title: "Refactoring", author: "Martin")
+      FactoryBot.create(:book, title: "sdewcf", author: "dwdwd")
+    end
+    it "returns all books" do
+      get "/api/v1/books"
 
-    get "/api/v1/books"
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
 
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(2)
+  describe "POST /books" do
+    it "creates a new book" do
+      expect {
+        post "/api/v1/books",
+             params: {
+               book: {
+                 title: "Refactoring",
+                 author: "Martin"
+               }
+             }
+      }.to change(Book, :count).from(0).to(1)
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe "DELETE /books/:id" do
+    let!(:book) do
+      FactoryBot.create(:book, title: "Refactoring", author: "Martin")
+    end
+    it "deletes a book" do
+      expect { delete "/api/v1/books/#{book.id}" }.to change(Book, :count).from(
+        1
+      ).to(0)
+      expect(response).to have_http_status(:no_content)
+    end
   end
 end
